305e25489ba672f09a10cb56c2b8fe104fe6fe4f
imon <andrey.kolotii@mail.ru>
2015-06-19 21:42:33 +0300

    Change js and html

--- index1.php
+++ index1.php
@@ -10,6 +10,21 @@ header("Content-Type: text/html; charset=utf-8");
     <title></title>
 </head>
 <body id="id">
-
+    <div>
+        <button id="addFieldToSearch">Добавить поля для поиска</button>
+        <select id="searchTag"></select>
+        <p>В каком разделе ищем</p>
+        <select id="city"></select>
+        <p>В каком городе ищем</p>
+        <table id="searchTable">
+            <tbody>
+                <tr>
+                    <td align="center">Что ищем</td>
+                    <td align="center">Чего не должно быть</td>
+                </tr>
+            </tbody>
+        </table>
+        <button id="sendDataToSearch">Отправить данные на обработку</button>
+    </div>
 </body>
 </html>
--- sum.js
+++ sum.js
@@ -1,17 +1,24 @@
 window.onload = function () {
-    var body = document.getElementById('id');
-    var buttonAddTr = document.createElement('button');
-    buttonAddTr.style.display='block'
-    var p = document.createElement('p');
-    var buttonSendForm = document.createElement('button');
-    var inputURL = document.createElement('input');
-    var table = document.createElement('table');
-    var tbody = document.createElement('tbody');
+    //var body = document.getElementById('id');
+    //var buttonAddTr = document.createElement('button');
+document.getElementById('addFieldToSearch').style.display='block'
+    //buttonAddTr.style.display='block'
+    //var searchTagP = document.createElement('p');
+    //var cityP = document.createElement('p');
+    //var buttonSendForm = document.createElement('button');
+    //var searchTag = document.createElement('select');
+    //var city = document.createElement('select');
+    //var searchOption = document.createElement('option');
+    //var searchOption2 = document.createElement('option');
+    //var cityOption = document.createElement('option');
+    //var cityOption2 = document.createElement('option');
+    //var table = document.createElement('table');
+    //var tbody = document.createElement('tbody');
     //var br = document.createElement('br');
-    var tr = document.createElement('tr');
-    var td1 = document.createElement('td');
-    var td2 = document.createElement('td');
-    body.appendChild(buttonAddTr);
+    //var tr = document.createElement('tr');
+    //var td1 = document.createElement('td');
+    //var td2 = document.createElement('td');
+    //body.appendChild(buttonAddTr);
     //document.body.firstElementChild.style.position='relative';
     //console.log(document.body.childNodes[2])
     //console.log(document.body.children[1])
@@ -20,30 +27,48 @@ window.onload = function () {
     //    console.log( document.body.children[i] ); // DIV, UL, DIV, SCRIPT
     //}
     //body.appendChild(br);
-    body.appendChild(inputURL);
-    body.appendChild(p);
-    p.innerText = 'Ссылка где ищем'
-    p.style.display = 'inline';
-    body.appendChild(table);
-    body.appendChild(buttonSendForm);
-    table.appendChild(tbody);
-    tbody.appendChild(tr);
-    tr.appendChild(td1);
-    tr.appendChild(td2);
-    buttonAddTr.setAttribute('id', 'addFieldToSearch');
-    buttonAddTr.innerText = 'Добавить поля для поиска';
-    buttonSendForm.setAttribute('id', 'sendDataToSearch');
-    buttonSendForm.innerText = 'Отправить форму';
-    inputURL.setAttribute('id','url');
-    inputURL.style.width='900px';
-    //inputURL.style.textOverflow='ellipsis';
+    //body.appendChild(searchTag);
+    //body.appendChild(searchTagP);
+    //body.appendChild(city);
+    //body.appendChild(cityP);
+
+    //searchTagP.innerText = 'В каком разделе ищем'
+    //searchTagP.style.display = 'inline';
+    //cityP.innerText = 'В каком городе ищем';
+    //cityP.style.display = 'inline';
+    //body.appendChild(table);
+    //body.appendChild(buttonSendForm);
+    //table.appendChild(tbody);
+    //tbody.appendChild(tr);
+    //tr.appendChild(td1);
+    //tr.appendChild(td2);
+    //buttonAddTr.setAttribute('id', 'addFieldToSearch');
+    //buttonAddTr.innerText = 'Добавить поля для поиска';
+    //buttonSendForm.setAttribute('id', 'sendDataToSearch');
+    //buttonSendForm.innerText = 'Отправить форму';
+    //searchTag.setAttribute('id','searchTag');
+    //searchTag.appendChild(searchOption);
+    //searchTag.appendChild(searchOption2);
+    //searchOption.innerText ='php';
+    //searchOption2.innerText ='java';
+
+    //city.setAttribute('id','city');
+    //city.appendChild(cityOption);
+    //cityOption.setAttribute('value','%D0%9D%D0%B8%D0%BA%D0%BE%D0%BB%D0%B0%D0%B5%D0%B2');
+    //cityOption2.setAttribute('value','%D0%9A%D0%B8%D0%B5%D0%B2&search');
+    //city.appendChild(cityOption2);
+    //cityOption.innerText = 'Николаев';
+    //cityOption2.innerText = 'Киев';
+    //searchTag.setAttribute('id','searchTag');
+    //searchTag.style.width='900px';
+    //searchTag.style.textOverflow='ellipsis';
 
     //
-    table.setAttribute('id', 'searchTable');
-    td1.innerText = 'Что ищем';
-    td1.setAttribute('align', 'center');
-    td2.innerText = 'Чего не должно быть';
-    td2.setAttribute('align', 'center');
+    //table.setAttribute('id', 'searchTable');
+    //td1.innerText = 'Что ищем';
+    //td1.setAttribute('align', 'center');
+    //td2.innerText = 'Чего не должно быть';
+    //td2.setAttribute('align', 'center');
 
     //console.log(document.getElementById('url').style.position='absolute');
     document.getElementById('addFieldToSearch').onclick = addFields;
@@ -105,11 +130,12 @@ window.onload = function () {
     function getAndSendSearchData() {
         var searchArray = getSearch();
         var notPresentedArray = getNotPresented();
-        var url = document.getElementById('url').value;
+        var searchTag = document.getElementById('searchTag').value;
+        var city = document.getElementById('city').value;
 
         var searchLength = searchArray.length;
         var searchDataArray = new Array();
-        searchDataArray[0] = url;
+        searchDataArray[0] = new Array(searchTag,city);
         for (i = 0; i < searchLength; i++) {
             searchDataArray[i+1] = {
                 "name": searchArray[i],
@@ -127,7 +153,7 @@ window.onload = function () {
 
         function showResult(data){
             data = JSON.parse(data);
-            //console.log(data)
+            console.log(data)
             for(var val in data){
                 //console.log(data[val]);
                 //console.log(val);
@@ -136,11 +162,17 @@ window.onload = function () {
                 div.innerText = val +" : " + data[val];
             }
         }
+        function consoleLogAjaxError(data){
+            data = JSON.parse(data);
+            //console.log(data.response);
+            console.log(data);
+        }
         var searchData = JSON.stringify(searchDataArray);
+        console.log(searchData);
         waitingForResponse();
         ajax('ajaxHandlers/searchQueryHandler.php',
             showResult,
-                false,
+                true,
                 'POST',searchData);
     }
 
@@ -174,49 +206,62 @@ window.onload = function () {
         return notPresentedArray;
     }
 
-    function ajax(url, callback, async, method, params, header)
+    function ajax(url, callback, type, method, params, header)
     {
-        //console.log(url)
-    //console.log(callback)
-    //console.log(async)
-    //console.log(method)
-    //    console.log(params);
         var xmlHttp = getXmlHttpRequest();
-        async = async || false;
-
-        params = params || '';
-        method = method || 'GET';
-        //console.log(params);
-
-        if (method == 'GET') url += '?'+'searchParams=' +params;
-        //console.log(url)
-        if (header != null) xmlHttp.setRequestHeader('Content-Type', header)
-        else if (method == 'POST')
-        {
-            //xmlHttp.setRequestHeader('"Content-Type: text/html;');
-            //header = 'application/x-www-form-urlencoded';
-            //xmlHttp.setRequestHeader('Content-Type', header)
-            //xmlHttp.responseType = 'json';
+        xmlHttp.onreadystatechange=function(){
+            if (xmlHttp.readyState==4 && xmlHttp.status==200)
+                callback(xmlHttp.response);
         }
-
-        xmlHttp.open(method, url, async);
-
-        if (!async)
-        {
-            if (params == '')xmlHttp.send(null); else xmlHttp.send(params);
-            callback(xmlHttp.responseText);
+        if(method=='POST') {
+            xmlHttp.open(method, url, type);
+            xmlHttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+        }else{
+            url += '?'+ params;
+            xmlHttp.open(method, url, type);
         }
-        else
-        {
-            xmlHttp.onreadystatechange = function() {
-                if (xmlHttp.readyState == 4) {
-                    callback(xmlHttp);
-                }
-            };
+        if(params!='') {
+            xmlHttp.send(params);
+        }else{
             xmlHttp.send(null);
         }
-        //return xmlHttp;
-    }
+        }
+    //    //return xmlHttp;    function ajax(url, callback, async, method, params, header)
+    //{
+    //    //console.log(url)
+    ////console.log(callback)
+    ////console.log(async)
+    ////console.log(method)
+    ////    console.log(params);
+    //    var xmlHttp = getXmlHttpRequest();
+    //    //console.log(params);
+    //    xmlHttp.open(method, url, async);
+    //
+    //    if (method == 'GET') url += '?'+'searchParams=' +params;
+    //    //console.log(url)
+    //    //if (header != null) xmlHttp.setRequestHeader('Content-Type', header)
+    //    if (method == 'POST')
+    //    {
+    //        xmlHttp.setRequestHeader('Content-type','application/x-www-form-urlencoded');
+    //    }
+    //
+    //
+    //    if (!async)
+    //    {
+    //        if (params == '')xmlHttp.send(null); else xmlHttp.send(params);
+    //        //callback(xmlHttp.responseText);
+    //    }
+    //    else
+    //    {
+    //        xmlHttp.onreadystatechange = function() {
+    //            if (xmlHttp.readyState == 4) {
+    //                callback(xmlHttp);
+    //            }
+    //        };
+    //        xmlHttp.send(null);
+    //    }
+    //    //return xmlHttp;
+    //}
 
     function waitingForResponse(){
         var body = document.body;
